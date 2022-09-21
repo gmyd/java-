939f70c47a97bf513e99e31f68cab546c2ec8f98
tayyibe <97178834+gmyd@users.noreply.github.com>
2022-09-21 22:08:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num=10;
+        String message="number of students: ";
+        System.out.println("number of students: "+num); //case sensitive (System)
+        System.out.println(message+num);
+    }
+}
