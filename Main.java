4be21908432956540c48827a398406fb3db44eac
tayyibe <97178834+gmyd@users.noreply.github.com>
2022-09-21 22:03:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double i=12.5;
+        int  j=4;
+        char character='s';
+        String s="hello";
+        boolean isItTrue=false;
+        
+    }
+}
