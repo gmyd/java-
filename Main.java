1d4f93e409561b10f82737c8faacb4802c8b5ee1
tayyibe <97178834+gmyd@users.noreply.github.com>
2022-09-21 22:04:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //FOR
+        for(int i=1;i<10;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("for finished");
+        //WHILE
+        int i=1;
+        while(i<10)
+        {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("while finished");
+        //DO-WHILE
+        int j=1;
+        do
+        {
+            System.out.println(j);
+            j++;
+        }while(j<10);
+        System.out.println("do-while finished");
+    }
+}
