628f7eb5b03c92cc6f2eb35fd59dc5380edccc1e
tayyibe <97178834+gmyd@users.noreply.github.com>
2022-09-21 22:03:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num=20;
+        if(num<20)
+        {
+            System.out.println("number less than 20");
+        }
+        else if(num>20)
+        {
+           System.out.println("number greater than 20");
+        }
+        else
+        {
+            System.out.println("number equal 20");
+        }
+    }
+}
